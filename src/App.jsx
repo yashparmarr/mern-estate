d4d768ae4b198de9056df30427a85a3e8c219875
Yash Parmar <yash20545@gmail.com>
2024-10-11 17:37:15 +0530

    Create header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/Signin" ;
 import SignUp from "./pages/SignUp" ;
 import About from "./pages/About" ;
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+   <Header/>
    <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
