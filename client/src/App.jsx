b12b67f6d043ff55161f59b179661421c923b717
Yash Parmar <yash20545@gmail.com>
2024-10-15 12:24:00 +0530

    complete sign up page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter , Route, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/Signin" ;
